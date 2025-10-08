0db61d661a52c2fd402fdf83d895c5db9d45919c
vega jafri <giantwhitedeer@gmail.com>
2025-10-08 19:08:55 -0400

    changes

--- systems-prototype-02/Assets/Scenes/scripts/PlayerMove.cs
+++ systems-prototype-02/Assets/Scenes/scripts/PlayerMove.cs
@@ -4,15 +4,25 @@ using System.Collections.Generic;
 
 public class PlayerMove : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public int moveSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxisRaw("Horizontal"))
+        {
+            rb.linearVelocity = new Vector2(moveSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(0, moveSpeed);
+        }
     }
 }
